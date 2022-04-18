79062ff0b93f93cac0e5206c3ed218419adba7c8
SDClowen <mahmutyildirim@outlook.com.tr>
2022-04-18 19:19:16 +0300

    Added button

---
+++ SDUI/Controls/Button.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDUI.Controls
+{
+    public class Button : Control
+    {
+        /// <summary>
+        /// Button raised color
+        /// </summary>
+        public Color Color { get; set; } = Color.Transparent;
+
+        /// <summary>
+        /// Mouse state
+        /// </summary>
+        private int _mouseState = 0;
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            _mouseState = 2;
+            Invalidate();
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            _mouseState = 1;
+            Invalidate();
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            _mouseState = 1;
+            Invalidate();
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            _mouseState = 0;
+            Invalidate();
+        }
+
+        public Button()
+        {
+            SetStyle(ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.SupportsTransparentBackColor, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+            var clientRectangle = new Rectangle(0, 0, Width - 1, Height - 1);
+            var innerRectangle = new Rectangle(1, 1, Width - 3, Height - 3);
+
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+            var color = ColorScheme.BackColor.Determine();
+
+            Brush gradient = null;
+            switch (_mouseState)
+            {
+                case 0:
+                    gradient = new SolidBrush(Color == Color.Transparent ? Color.FromArgb(45, color) : Color);
+                    break;
+
+                case 1:
+                    gradient = new SolidBrush(Color == Color.Transparent ? Color.FromArgb(60, color) : Color.FromArgb(180, Color));
+                    break;
+
+                case 2:
+                    gradient = new SolidBrush(Color == Color.Transparent ? Color.FromArgb(80, color) : Color.FromArgb(220, Color));
+                    break;
+            }
+
+            var outerPen = new Pen(Color == Color.Transparent ? ColorScheme.BorderColor : Color.FromArgb(95, Color.Determine()));
+
+            graphics.FillRectangle(gradient, clientRectangle);
+            gradient.Dispose();
+
+            graphics.DrawRectangle(outerPen, clientRectangle);
+            outerPen.Dispose();
+
+            var textRectangle = new Rectangle(0, 1, Width - 1, Height - 1);
+            TextRenderer.DrawText(graphics, Text, Font, textRectangle, Color == Color.Transparent ? ColorScheme.ForeColor : ForeColor, TextFormatFlags.EndEllipsis | TextFormatFlags.VerticalCenter | TextFormatFlags.HorizontalCenter);
+        }
+    }
+}
