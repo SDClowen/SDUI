c15edae3592b9c922b21d5943f844ef23fe98026
SDClowen <mahmutyildirim@outlook.com.tr>
2022-04-21 05:51:45 +0300

    ...

--- SDUI/Controls/Panel.cs
+++ SDUI/Controls/Panel.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using SDUI.Extensions;
+using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using SDUI.Extensions;
 
 namespace SDUI.Controls
 {
-    public class Panel : System.Windows.Forms.Control
+    public class Panel : System.Windows.Forms.Panel
     {
         private int _radius = 12;
         public int Radius
